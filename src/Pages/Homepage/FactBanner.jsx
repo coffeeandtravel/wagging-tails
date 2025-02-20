32a93c4afdc3edd4df57c13e2a67e7b48408a610
Mayank Gaur <85687789+coffeeandtravel@users.noreply.github.com>
2025-02-20 18:26:50 +0530

    mobile

--- src/Pages/Homepage/FactBanner.jsx
+++ src/Pages/Homepage/FactBanner.jsx
@@ -1,18 +1,18 @@
 import "./Banner.css";
-import fact from '../../assets/fact.jpg'
+// import fact from '../../assets/fact.jpg'
 const FactBanner = () => {
   return (
-    <div className="hidden md:block mt-5">
+    <div className=" mt-5">
       <div className=" flex items-center justify-center">
-        <div className="h-[600px] w-[906px] bg-black rounded-3xl flex justify-between items-center">
-          <div className="flex flex-col ml-10 gap-50 max-w-1/2">
-            <h2 className="text-3xl text-white">
-              Over 30 Million Stray Dogs <br /> Roam the Streets of India.
-            </h2>
-            <p className="text-white text-justify">{`"In India, millions of stray dogs struggle to survive on the streets, often facing hunger, illness, and the dangers of traffic. Despite their tough lives, many of these resilient dogs are incredibly loving and loyal, waiting for a chance to find a safe home. By adopting a stray dog, you’re not only saving a life but also giving them the love and care they deserve. Adoption is a powerful way to reduce the homeless dog population and provide these animals with the opportunity for a better future."`}</p>
+        <div className="h-[900px] w-[94%] lg:h-[600px] lg:w-[906px] bg-black rounded-3xl flex items-center mb-12 p-5 flex-col lg:flex-row-reverse justify-between">
+          <div className="fact h-[500px] w-full mt-3 rounded-3xl flex items-center justify-center">
+            <div className="h-[440px] w-11/12 linear rounded-3xl flex gap-45 flex-col">
+            <p className="text-xl pl-4 pt-5 banner-heading">Over 30 Million Stray Dogs <br /> Roam the Streets of India</p>
+            <p className="text-xl pl-4 pt-5 banner-heading">Give them a <br /> Loving Home</p>
+            </div>
           </div>
-          <div className="w-1/2 h-full flex justify-center items-center">
-            <img src={fact} alt="" className="h-[85%] rounded-3xl" />
+          <div>
+            <p className="text-white banner-heading">{`"In India, millions of stray dogs struggle to survive on the streets, often facing hunger, illness, and the dangers of traffic. Despite their tough lives, many of these resilient dogs are incredibly loving and loyal, waiting for a chance to find a safe home. By adopting a stray dog, you’re not only saving a life but also giving them the love and care they deserve. Adoption is a powerful way to reduce the homeless dog population and provide these animals with the opportunity for a better future."`}</p>
           </div>
         </div>
       </div>
